07cd169518eaa1d86b114bac47b2a9b26dc40e52
mbilawalzaman <mbilawalzaman@gmail.com>
2024-03-18 05:02:37 +0500

    get all orders by Id api created

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -649,6 +649,7 @@ router.get("/getAllOrdersByUserId/:userId", async (req, res) => {
     const userId = req.params.userId;
     const orders = await Order.find({ userId });
     res.status(200).json({ orders });
+    
   } catch (error) {
     console.error("Error fetching orders:", error);
     res.status(500).json({ message: "Internal Server Error" });
